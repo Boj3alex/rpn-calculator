146775af78b4f7bd9f2d3fff70c7b454fee38203
Boj3alex <32925802+Boj3alex@users.noreply.github.com>
2020-08-25 03:26:48 +0300

    Create rpn_evaluation_tests.py
    added tests for rpn evaluation program

---
+++ tests/rpn_evaluation_tests.py
@@ -0,0 +1,37 @@
+import unittest
+from app.evaluation import rpn_evaluation
+
+
+class RPNNotationCalculatorTests(unittest.TestCase):
+
+    def test_no_input(self):
+        self.assertEqual(rpn_evaluation(''), 0)
+
+    def test_simple_addition(self):
+        self.assertEqual(rpn_evaluation('2 3 +'), 5)
+
+    def test_simple_division(self):
+        self.assertEqual(rpn_evaluation('5 2 /'), 2)
+
+    def test_multiplication_and_addition(self):
+        self.assertEqual(rpn_evaluation('4 7 + 2 *'), 22)
+
+    def test_multiplication_and_addition_complex(self):
+        self.assertEqual(rpn_evaluation('2 3 * 11 14 * +'), 160)
+
+    def test_modulo_and_multiplication(self):
+        self.assertEqual(rpn_evaluation('8 4 % 10 +'), 10)
+
+    def test_invalid_operator(self):
+        with self.assertRaises(Exception) as details:
+            rpn_evaluation('30 13 &')
+        self.assertTrue('Invalid character' in str(details.exception))
+
+    def test_invalid_operand(self):
+        with self.assertRaises(Exception) as details:
+            rpn_evaluation('25 10 * 4 t + +')
+        self.assertTrue('Invalid character' in str(details.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
