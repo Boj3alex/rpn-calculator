705c21e250a1105ae02ab4e620546e77fd1d805f
Boj3alex <32925802+Boj3alex@users.noreply.github.com>
2020-08-31 20:46:26 +0300

    added exception handling feature

--- app/evaluation.py
+++ app/evaluation.py
@@ -1,10 +1,6 @@
-import argparse
+import re
 
-parser = argparse.ArgumentParser(description="RPN expression calculator - options")
-parser.add_argument('--expr', help='Type the RPN expression for evaluation inside quotes', required=False)
-
-args = parser.parse_args()
-
+floating_point_regex = '[0-9]*\.[0-9]*'
 
 def do_operation(element1, element2, operator):
     if operator == '+':
@@ -23,18 +19,26 @@ def rpn_evaluation(rpn_exp):
     results_list = []
     operator_list = ['+', '-', '*', '/', '%']
 
-    for element in rpn_exp.split():
-        if element in operator_list:
-            operator2 = results_list.pop()
-            operator1 = results_list.pop()
-            results_list.append(do_operation(operator1, operator2, element))
-        elif element.isnumeric():
-            results_list.append(int(element))
-        else:
-            raise Exception('Invalid character')
+    try:
+        for element in rpn_exp.split():
+            if element in operator_list:
+                operator2 = results_list.pop()
+                operator1 = results_list.pop()
+                results_list.append(do_operation(operator1, operator2, element))
+            elif element.isnumeric():
+                results_list.append(int(element))
+            elif re.search(floating_point_regex, element):
+                raise Exception('Floating-point numbers are not accepted.')
+            else:
+                raise Exception('Invalid character')
+    except IndexError:
+        print('Invalid RPN expression')
 
     return results_list.pop() if len(results_list) > 0 else 0
 
 
 if __name__ == '__main__':
-    print('The result of the RPN expression is:', rpn_evaluation(args.expr))
+    print('Type the RPN expression that you want to evaluate:')
+    rpn_exp = input()
+
+    print('The result of the RPN expression is:', rpn_evaluation(rpn_exp))
